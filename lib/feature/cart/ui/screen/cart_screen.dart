4b64adef7a048ca08313fbbd2eef3b75361c7d2d
Youssef Ramadan <79144849+YoussefElqursh@users.noreply.github.com>
2025-07-26 20:05:42 +0300

    add order details in cart screen

--- lib/feature/cart/ui/screen/cart_screen.dart
+++ lib/feature/cart/ui/screen/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:restero/feature/cart/logic/cart_cubit.dart';
 
 class CartScreen extends StatelessWidget {
@@ -11,7 +12,18 @@ class CartScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cart'),
+        centerTitle: true,
+        elevation: 0.3,
+        title: const Text(
+          'Cart',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        leading: SizedBox.shrink(),
         actions: [
           IconButton(
             icon: const Icon(Icons.clear),
@@ -44,28 +56,150 @@ class CartScreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       final cartItem = cartItems[index];
                       return ListTile(
-                        leading: Image.network(
-                          cartItem.item.imageUrl,
-                          height: 50,
+                        leading: Container(
                           width: 50,
+                          height: 50,
+                          decoration: ShapeDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(cartItem.item.imageUrl),
+                              fit: BoxFit.cover,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                width: 1,
+                                strokeAlign: BorderSide.strokeAlignCenter,
+                                color: Color(0xFFEBEBEB),
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
                         ),
-                        title: Text(cartItem.item.name),
+                        title: Text(
+                          cartItem.item.name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
                         subtitle: Text(
                           '\$${cartItem.item.price.toStringAsFixed(2)}',
+                          style: const TextStyle(fontWeight: FontWeight.normal),
                         ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.remove),
-                          onPressed: () {
-                            context.read<CartCubit>().removeItem(
-                              cartItem.item.itemId,
-                            );
-                          },
+                        trailing: Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xFFDFD2C9),
+                            borderRadius: BorderRadius.circular(8.0.r),
+                          ),
+                          child: IconButton(
+                            style: IconButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0.r),
+                              ),
+                              backgroundColor: Color(0xFFDFD2C9),
+                              foregroundColor: Color(0xFFCC5920),
+                              elevation: 0,
+                              hoverColor: Colors.transparent,
+                              focusColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                            ),
+                            icon: const Icon(
+                              Icons.remove,
+                              color: Color(0xFFCC5920),
+                            ),
+                            onPressed: () {
+                              context.read<CartCubit>().removeItem(
+                                cartItem.item.itemId,
+                              );
+                            },
+                          ),
                         ),
                       );
                     },
                   ),
           ),
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Item Total:'),
+                  Text(
+                    '${cartItems.length}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Total: '),
+                  Text(
+                    '\$${cartItems.fold<double>(0, (total, cartItem) => total + cartItem.item.price)}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFCC5920),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Inter',
+                      ),
+                      minimumSize: const Size(150, 50),
+                      maximumSize: const Size(double.infinity, 50),
+                      elevation: 0,
+                      shadowColor: Colors.transparent,
+                      side: const BorderSide(color: Colors.white, width: 2),
+                      alignment: Alignment.center,
+                      visualDensity: VisualDensity.comfortable,
+                    ),
+                    onPressed: () {},
+                    child: const Text('Checkout'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
